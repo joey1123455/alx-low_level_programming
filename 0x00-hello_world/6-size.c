7186577afcfe54f773dce7b9533bd0511dadfd62
Joey <folayanjoey@gmail.com>
2023-02-16 22:32:55 +0100

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+/**
+ * main - prints the sizes of data types in c
+ * @ s_int - size of int 
+ * @ s_dbl - size of double
+ * @ s_flt - size of floats
+ * @ s_chr - size of char
+ * 
+ * return 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int s_int = sizeof(int);
+  int s_dbl = sizeof(double);
+  int s_flt = sizeof(float);
+  int s_chr = sizeof(char);
+
+  printf("size of int type is %d \n", s_int);
+  printf("size of double type is %d\n", s_dbl);
+  printf("size of float type is %d\n", s_flt);
+  printf("size of char type is %d\n", s_chr);
+
+  return (0);
+}
