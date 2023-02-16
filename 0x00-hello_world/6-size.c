bfb270af88a70cbaf9365ff6858659ae46fbcd75
Joey <folayanjoey@gmail.com>
2023-02-16 23:35:06 +0100

    betty formatting

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	printf("Size of char is: %ld byte(s)\n", sizeof(char));
-	printf("Size of int is: %ld byte(s)\n", sizeof(int));
-	printf("Size of long int is: %ld bytes(s)\n", sizeof(long int));
-	printf("Size of long long int is: %ld bytes(s)\n", sizeof(long long int));
-	printf("Size of float is: %ld bytes(s)", sizeof(float));
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+	printf("Size of int: %ld byte(s)\n", sizeof(int));
+	printf("Size of long int: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of long long int: %ld bytes(s)\n", sizeof(long long int));
+	printf("Size of float: %ld bytes(s)", sizeof(float));
 	return (0);
 }
