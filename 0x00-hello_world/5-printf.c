5b2cf111cf91b169f483ac16f754aca91c5aafec
Joey <folayanjoey@gmail.com>
2023-02-16 23:19:58 +0100

    betty formatting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 /**
- *main - prints a string onto the screen
+ * main - prints a string onto the screen
  *
- *Return : always 0
+ * Return : Always 0 (Successs)
 */
-
 #include <stdio.h>
-
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art, \n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 
 	return (0);
 }
