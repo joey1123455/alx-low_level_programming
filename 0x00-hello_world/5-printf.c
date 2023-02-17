312a35e09f857fdd82071c9a8cc12ddc9c9b2671
Joey <folayanjoey@gmail.com>
2023-02-17 02:46:20 +0100

    betty formatting

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string using printf
+ *
+ * Returns: o if succesful
+ */
+int main() 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
