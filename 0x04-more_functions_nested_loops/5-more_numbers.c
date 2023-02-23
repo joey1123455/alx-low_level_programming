e9335977814dce5877bf7095eabd8af6432e1b6a
Joey <folayanjoey@gmail.com>
2023-02-23 18:33:34 +0100

    more no & most no

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if (i > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 		i++;
