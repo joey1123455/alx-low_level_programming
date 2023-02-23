d09d968d7ffbe1ab00ffa8695144e053d901602c
Joey <folayanjoey@gmail.com>
2023-02-23 03:28:17 +0100

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,11 +8,11 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 2; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			for (k = 0; k <= 9; k++)
+			for (k = 0; k <= 5; k++)
 			{
 				for (l = 0; l <= 9; l++)
 				{
