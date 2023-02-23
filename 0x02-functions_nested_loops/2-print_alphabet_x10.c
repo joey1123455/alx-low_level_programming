4797f6bd773a6ec5f1422e9c2bd1f109028ef600
Joey <folayanjoey@gmail.com>
2023-02-23 01:54:51 +0100

    10x lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+/**
+* print_alphabet_x10 - prints 10 sets of lower case letters
+*
+* Return: void
+*/
+
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	}
+}
