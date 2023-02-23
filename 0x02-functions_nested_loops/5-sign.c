def2f5149fe4b83623748759909ab9ae83afed24
Joey <folayanjoey@gmail.com>
2023-02-23 02:40:21 +0100

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+* print_sign - prints the sign of an int, either negative or positive
+* @n: the integer to be checked
+*
+* Return: 1 positive else return 0 or -1
+*/
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
