f08eaf1aa15f0f010db8f96e900ff0e2d3648930
Joey <folayanjoey@gmail.com>
2023-02-23 17:24:49 +0100

    print no & mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/**
+* mul - multiply 2 ints
+* @a: first int
+* @b: second int
+*
+* Return: a * b
+*/
+
+#include "main.h"
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+* print_numbers - prints char 0 - 9
+* 
+* Return: void
+*/
+
+#include "main.h"
+
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i);
+	_putchar('\n');
+}
