c98d8b077911a9ec852029160da26b857bec57e3
Joey <folayanjoey@gmail.com>
2023-02-23 03:50:35 +0100

    primary school memoirs

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+/**
+* times_table - print 9 times table
+*/
+
+#include "main.h"
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if ((k / 10) == 0)
+			{
+				_putchar((k % 10) + '0');
+			} else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if ((k / 10) == 0)
+					_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
