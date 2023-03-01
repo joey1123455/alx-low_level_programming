1d3b7e1654a258888f813faeeeaf744a17c3f16e
Joey <folayanjoey@gmail.com>
2023-03-01 02:35:32 +0100

    push

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,9 @@ void print_rev(char *s)
 	int x;
 	int y;
 
-	for (x = 0; s[x] != '\0'; x++)
-	{
-		for (y = x - 1; y >= 0; y--)
-			_putchar(s[y]);
-	}
+	while (s[x] != '\0')
+		b++;
+	for (y = x - 1; y >= 0; y--)
+		_putchar(s[y]);
 	_putchar('\n');
 }
