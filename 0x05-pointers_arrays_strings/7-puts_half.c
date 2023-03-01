0d54968a1958ddaa72057cbe2f22afb946c53091
Joey <folayanjoey@gmail.com>
2023-03-01 03:55:50 +0100

    push

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,36 +11,14 @@ void puts_half(char *str)
 {
 	int x, y;
 
-	y = _strlen(str);
-	if (y % 2 == 0)
+	y = 0;
+	if (*(str + y) != '\0')
+		y++;
+	x = y % 2 == 0 ? y / 2 : (l / 2) + 1;
+	if (x < y)
 	{
-		x = y / 2;
-	} else
-	{
-		x = (y + 1) / 2;
-	}
-	while (str[x] != '\0')
-	{
-		_putchar(str[x]);
+		_putchar(*(str + x));
 		x++;
 	}
 	_putchar('\n');
 }
-
-/**
-* _strlen - len of str
-* @s: str
-*
-* Return: len of str
-*/
-
-int _strlen(char *s)
-{
-	int x;
-	int y = 0;
-
-	for (x = 0; s[x] != '\n'; x++)
-		y += 1;
-	return (y);
-}
-
