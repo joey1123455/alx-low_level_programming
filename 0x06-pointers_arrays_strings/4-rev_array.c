4dbcd95d8030bcadc14433f5e6272435bc7df4f3
Joey <folayanjoey@gmail.com>
2023-03-04 02:34:40 +0100

    push

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,9 +13,9 @@ void reverse_array(int *a, int n)
 	int t;
 	int i;
 
-	for (i = n - 1; i >= n / 2; i--)
+	for (i = 0; i >= n / 2; i--)
 	{
-		t = a[n - 1 - index];
+		t = a[n - 1 - i];
 		a[n - 1 -i] = a[i];
 		a[i] = t;
 	}
