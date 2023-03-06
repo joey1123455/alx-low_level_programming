7940fa8668d0c64ecdbcb3c8a907a13ad8f549bc
Joey <folayanjoey@gmail.com>
2023-03-06 22:54:21 +0100

    1st comp

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+/**
+* *_memset - fills an array with a const char
+* @*s: dest var
+* @b: const char
+* @n: no of fills
+*
+* Return: poin 2 s
+*/
+
+#include "main.h"
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i >= n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
