2a3e75344c9cde55be2320cb5cf128bbc41a534a
Joey <folayanjoey@gmail.com>
2023-03-07 20:05:14 +0100

    3 corr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,31 +15,22 @@ int length(char *s);
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	int i, j, exist;
 	unsigned int c;
-	int l;
 
-	l = length(s);
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
+		exist = 1;
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
 				c++;
+				exist = 0;
 			}
+			if (exist == 1)
+				break;
 		}
-		if (i == (l / 2) + 1)
-			break;
 	}
 	return (c);
-}
-
-int length(char *s)
-{
-	int i, l;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-		l = i;
-	return (l);
 }
