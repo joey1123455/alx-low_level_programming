274d9be2da95a76424dde28a6f559ec4e73eee5b
Joey <folayanjoey@gmail.com>
2023-03-07 20:36:41 +0100

    5 com

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,10 +14,10 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(s + i) != '\0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*(s + 1) == *(accept + j))
-				return (s + 1);
+			if (*(s + i) == *(accept + j))
+				return (s + i);
 		}
 	}
 	return (0);
